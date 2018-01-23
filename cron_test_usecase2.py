3b97298d465683fb840175ff510eddd567e57348
sanjaypursuite <sanjay@pursuite.com>
2018-01-23 22:26:58 +0530

    removed print stmt

--- cron_test_usecase2.py
+++ cron_test_usecase2.py
@@ -4,7 +4,6 @@ import sys
 
 
 import google
-print google.__path__
 from google.cloud import bigquery
 client = bigquery.Client('indigo-lotus-154020')
 query = '(SELECT *FROM (SELECT *,ROW_NUMBER() OVER (PARTITION BY po_id ORDER BY po_updated_date DESC) AS RowNo FROM (select * from (SELECT A.po_id,A.buyer_company_name,A.delivery_state, A.po_amount, A.po_updated_date, B.seller_company_name, B.managed_type, B.operating_company, B.property_category FROM `indigo-lotus-154020.nodejs.po_updated_report_data` A left join `indigo-lotus-154020.nodejs.po_product_updated_report_data` B on A.po_id = B.po_id order by A.po_updated_date desc) AS C) ) x WHERE x.RowNo = 1)'
